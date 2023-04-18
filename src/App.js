1cce7393ea8be8cb5fa0b02c8c7f8dbe18d0a4a6
Franklin Osigwe <franklindutch1@gmail.com>
2023-04-18 13:42:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import  "./App.css";
-import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
-import Home from "./components/Home";
-import NotFound from "./components/NotFound";
-
+import React from 'react'
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <ToastContainer/>
-      <NavBar />
-        <Routes>
-        <Route exact path="/cart" element={<Cart/>}/>
-        <Route exact path="/not-found" element={<NotFound/>}/>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-   
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
